ac531c2c2906cee91903e3e9c77c6375bfe56df3
Raymond Youssef <rimonomega@gmail.com>
2021-01-04 00:37:58 +0200

    Fixed Angular

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'ray-coffee-shop.eu.auth0.com', // the auth0 domain prefix
+    url: 'ray-coffee-shop.eu', // the auth0 domain prefix
     audience: 'Coffee Shop', // the audience set for the auth0 app
     clientId: '0TDHLPYUb1MSjGr4jfdYyjhmu96MERZw', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application
